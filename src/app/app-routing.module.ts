d8c6ad3e5076f561cb823989a24e57741e143b0c
Mansour <ilyes.mansour@sap.com>
2020-08-14 11:08:52 +0200

    Add guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,14 @@ import { SignupComponent } from './auth/signup/signup.component';
 import { BookListComponent } from './book-list/book-list.component';
 import { ViewBookComponent } from './book-list/view-book/view-book.component';
 import { CreateBookComponent } from './book-list/create-book/create-book.component';
+
+import { AuthGuardService } from './service/auth-guard.service';
+
 const routes: Routes = [{path:"auth/signin", component:SigninComponent},
-{path:"auth/signup",component:SignupComponent},
-{path:"books",component: BookListComponent},
-{path:"books/view/:id",component:ViewBookComponent},
-{path:"books/create",component: CreateBookComponent},
+{path:"auth/signup", component:SignupComponent},
+{path:"books", canActivate:[AuthGuardService], component: BookListComponent},
+{path:"books/view/:id", canActivate:[AuthGuardService], component:ViewBookComponent},
+{path:"books/create", canActivate:[AuthGuardService], component: CreateBookComponent},
 {path:"", redirectTo:"books", pathMatch:"full"},
 {path:"**", redirectTo:"books"}];
 
--- src/app/service/auth-guard.service.ts
+++ src/app/service/auth-guard.service.ts
@@ -1,9 +1,35 @@
-import { Injectable } from '@angular/core';
-
+import { Injectable, resolveForwardRef } from '@angular/core';
+import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import * as firebase from 'firebase';
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuardService {
+export class AuthGuardService implements CanActivate {
 
-  constructor() { }
+  constructor(private router: Router) { }
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | import("@angular/router").UrlTree | import("rxjs").Observable<boolean | import("@angular/router").UrlTree> | Promise<boolean | import("@angular/router").UrlTree> {
+
+
+    return new Promise((resolve, reject) => {
+
+
+      firebase.auth().onAuthStateChanged(
+        (user) => {
+
+          if (user) {
+            resolve(true);
+          }
+          else { resolve(false); 
+          this.router.navigate(['/auth','signin']);
+          }
+
+        },
+        (error) => { reject(error); }
+
+      );
+
+
+    });
+
+  }
 }
